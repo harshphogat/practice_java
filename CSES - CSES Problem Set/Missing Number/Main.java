0bd5d8252a3956a8f6d7e018a22c2642c919b6c5
harshphogat <55057156+harshphogat@users.noreply.github.com>
2022-09-17 22:29:33 +0530

    Missing Number Heap_Sort approach

--- CSES - CSES Problem Set/Missing Number/Main.java
+++ CSES - CSES Problem Set/Missing Number/Main.java
@@ -1,5 +1,6 @@
 //  Missing Number
 
+
 import java.lang.*;
 import java.io.*;
 class Main {
@@ -16,36 +17,39 @@ class Main {
             System.out.println(1);
         }
         else {
-            quick_sort(arr, 0, n - 2);
-            int ans = binary_search(arr, 0, n - 2);
-            System.out.println(ans);
-        }
-    }
-    public static int partition(int [] num, int low, int len){
-        int pivot = num[len];
-        int i = low-1;
-        for (int j = low; j < len; j++) {
-            if (num[j] <= pivot) {
-                i += 1;
-                int temp = num[i];
-                num[i] = num[j];
-                num[j] = temp;
-            }
-        }
-        int temp = num[i+1];
-        num[i+1] = num[len];
-        num[len] = temp;
-        return i+1;
-    }
+            int ans = heap_sort(arr,n-2);
+//            quick_sort(arr, 0, n - 2);
+//            int ans = binary_search(arr, 0, n - 2);
+//            for (int i =0; i<=n-2; i++){
+                System.out.println(ans);
 
-    public static void quick_sort(int [] num, int low, int len){
-        if (low<len){
-            int q = partition(num, low, len);
-            quick_sort(num, low, q-1);
-            quick_sort(num, q+1, len);
         }
     }
-
+//    public static int partition(int [] num, int low, int len){
+//        int pivot = num[len];
+//        int i = low-1;
+//        for (int j = low; j < len; j++) {
+//            if (num[j] <= pivot) {
+//                i += 1;
+//                int temp = num[i];
+//                num[i] = num[j];
+//                num[j] = temp;
+//            }
+//        }
+//        int temp = num[i+1];
+//        num[i+1] = num[len];
+//        num[len] = temp;
+//        return i+1;
+//    }
+//
+//    public static void quick_sort(int [] num, int low, int len){
+//        if (low<len){
+//            int q = partition(num, low, len);
+//            quick_sort(num, low, q-1);
+//            quick_sort(num, q+1, len);
+//        }
+//    }
+//
     public static int binary_search(int [] num, int low, int high){
         while(low<high) {
             int mid = (low + high + 1)/2;
@@ -55,5 +59,38 @@ class Main {
                 high = mid-1;
         }
         return num[low]+1;
+    }
+    public static void heapify(int [] arr, int len, int i){
+        int largest = i;
+        int l = 2*i;
+        int r = 2*i+1;
+        if (l<=len && arr[l] > arr[largest])
+            largest = l;
+        if (r<=len && arr[r] > arr[largest])
+            largest = r;
+        if(largest != i){
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, len, largest);
+        }
+    }
+    public static int heap_sort(int [] arr, int len){
+        int size = len;
+        for (int i =len/2; i>=0; i--){
+            heapify(arr, len, i);
+        }
+        for(int i = len; i>0; i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            size -= 1;
+            if(arr[i]-i ==1){
+                return arr[i]+1;
+            }
+            else
+                heapify(arr, size,   0);
+        }
+        return 2;
     }
 }
